193db97edec647d068868a0277f50ee387fa5943
k <raphhawk>
2022-12-16 19:05:51 +0530

    adding Postgres

---
+++ authentication-service/data/models.go
@@ -0,0 +1,195 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"time"
+)
+
+const dbTimeout = time.Second * 3
+
+var db *sql.DB
+
+func New(dbPool *sql.DB) Models {
+	db = dbPool
+
+	return Models{
+		User: User{},
+	}
+}
+
+type Models struct {
+	User User
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	Email     string    `json:"email"`
+	FirstName string    `json:"first_name,omitempty"`
+	LastName  string    `json:"last_name,omitempty"`
+	Password  string    `json:"-"`
+	Active    int       `json:"active"`
+	CreatedAt time.Time `json:"created_at"`
+	UpadtedAt time.Time `json:"updated_at"`
+}
+
+func (u *User) GetAll() ([]*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select 
+		id,
+		email,
+		first_name,
+		last_name,
+		password,
+		user_active,
+		created_at,
+		updated_at
+	from users order by last_name`
+
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []*User
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(
+			&user.ID,
+			&user.Email,
+			&user.FirstName,
+			&user.LastName,
+			&user.Password,
+			&user.Active,
+			&user.CreatedAt,
+			&user.UpadtedAt,
+		)
+
+		if err != nil {
+			log.Println("Error scanning", err)
+			return nil, err
+		}
+
+		users = append(users, &user)
+	}
+	return users, nil
+}
+
+func (u *User) GetByEmail(email string) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select 
+		id,
+		email,
+		first_name,
+		last_name,
+		password,
+		user_active,
+		created_at,
+		updated_at
+	from users where email=${email}`
+
+	var user User
+	row := db.QueryRowContext(ctx, query, email)
+
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.Active,
+		&user.CreatedAt,
+		&user.UpadtedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (u *User) GetOne(id int) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select 
+		id,
+		email,
+		first_name,
+		last_name,
+		password,
+		user_active,
+		created_at,
+		updated_at
+	from users where id=${id}`
+
+	var user User
+	row := db.QueryRowContext(ctx, query, id)
+
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.Active,
+		&user.CreatedAt,
+		&user.UpadtedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (u *User) Update() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `update users set
+		email = $1, 
+		first_name = $2, 
+		last_name = $3,
+		user_active = $4,
+		updated_at = $5,
+		where id = $6
+	`
+	_, err := db.ExecContext(
+		ctx,
+		stmt,
+		u.Email,
+		u.FirstName,
+		u.LastName,
+		u.Active,
+		time.Now(),
+		u.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *User) Delete() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `delete from users where id = $1`
+
+	_, err := db.ExecContext(ctx, stmt, u.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ authentication-service/go.mod
@@ -0,0 +1,3 @@
+module github.com/raphhawk/auth
+
+go 1.19
