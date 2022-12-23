552324502b06c81ee46ac940920d2b317abebe22
k <raphhawk>
2022-12-23 12:19:37 +0530

    broker-auth2

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"net/http"
 )
@@ -15,11 +17,6 @@ func (app *Config) Broker(
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
-
-	// out, _ := json.MarshalIndent(payload, "", "\t")
-	// w.Header().Set("Content-Type", "application/json")
-	// w.WriteHeader(http.StatusAccepted)
-	// w.Write(out)
 }
 
 type AuthPayload struct {
@@ -30,6 +27,61 @@ type AuthPayload struct {
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+}
+
+func (app *Config) authenticate(
+	w http.ResponseWriter,
+	a AuthPayload,
+) {
+	// create json data to be send to auth service
+	jsonData, _ := json.MarshalIndent(a, "", "\t")
+
+	// call the service
+  request, err := http.NewRequest(
+    "POST",
+    "http://authentication-service/authenticate",
+    bytes.NewBuffer(jsonData),
+  )
+  if err != nil {
+    app.errorJSON(w, err)
+    return
+  }
+
+  client := &http.Client{}
+  response, err := client.Do(request)
+  if err != nil {
+    app.errorJSON(w, err)
+    return
+  }
+  defer response.Body.Close()
+
+	// make sure we get back the correct status code
+  if response.StatusCode == http.StatusUnauthorized {
+    app.errorJSON(w, errors.New("invalid credentials"))
+    return
+  } else if response.StatusCode != http.StatusAccepted {
+    app.errorJSON(w, errors.New("error calling auth service"))
+    return
+  }
+
+  var jsonFromService jsonResponse
+  err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+  if err != nil {
+    app.errorJSON(w, err)
+    return
+  }
+
+  if jsonFromService.Error {
+    app.errorJSON(w, err, http.StatusUnauthorized)
+    return
+  }
+
+  var payload jsonResponse 
+  payload.Error = false
+  payload.Message = "Authenticated!"
+  payload.Data = jsonFromService.Data 
+
+  app.writeJSON(w, http.StatusAccepted, payload)
 }
 
 func (app *Config) HandleSubmission(
