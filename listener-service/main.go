2143a850278315227556d1bacf793cefd2845134
k <bing619.at@gmail.com>
2023-01-06 23:37:06 +0530

    listener1

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,7 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	backoff := 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backoff = (time.Duration(math.Pow(float64(backoff), 2)) * time.Second)
+		log.Println("backing off..")
+		time.Sleep(backoff)
+		continue
+	}
+	return connection, nil
+}
+
 func main() {
 	//connect to rabbit-mq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
 	//start listening for messages
 	//create consumer
 	//watch queue and consume events
