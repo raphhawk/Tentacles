34c6f2b75e0a4603b415287dc0dd8a1560632eec
k <bing619.at@gmail.com>
2023-01-06 23:02:26 +0530

    adding listener

---
+++ listener-service/go.mod
@@ -0,0 +1,5 @@
+module github.com/raphhawk/listener-service
+
+go 1.19
+
+require github.com/rabbitmq/amqp091-go v1.5.0 // indirect
---
+++ listener-service/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	//connect to rabbit-mq
+	//start listening for messages
+	//create consumer
+	//watch queue and consume events
+}
